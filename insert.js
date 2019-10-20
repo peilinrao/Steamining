a92f480e17e3ee10a2dda1197e4a6b3bd27f2c15
Peilin Rao <pelinium@gmail.com>
2019-10-19 22:13:23 -0500

    OK

---
+++ insert.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'UIuc7355608!!',
+  database:'steamining_main'
+});
+connection.connect();
+
+var Users = {
+
+}
