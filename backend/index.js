a5c9a34dc22d10d63d112bd6ca9ad0ffac06f015
Yitao Meng <yitaom2@illinois.edu>
2019-10-30 19:27:37 -0500

    a

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,19 @@ var users_table = require('./users_table');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+//
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  multipleStatements: true,
+  host:'localhost',
+  user:'root',
+  password:'password',
+  database:'STEAMINING_MAIN'
+});
+
+con.connect(err => {if(err) {return err;}})
+//
 app.use(cors());
 
 app.get('/',(req,res)=>{
@@ -14,6 +27,19 @@ app.get('/add_into_database',(req,res)=>{
   users_table.API_users_add(steamid);
   res.send("You are added!");
 })
+
+//
+app.get('/show', (req, res) => {
+  const {steamid} = req.query;
+  const VIEW_GAMES_FOR_ID_QUERY = "SELECT * FROM STEAMINING_MAIN.USERS WHERE SteamId64 = " + steamid + ";"
+  con.query(VIEW_GAMES_FOR_ID_QUERY, (err, results) => {
+    if(err) {return res.send(err)}
+    else {
+      return res.json({data: results})
+    }
+  });
+})
+//
 
 app.listen(4000,()=>{
   console.log('backend listening on 4000');
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    steamid: '11111111111111111',
+    usergames: []
+  }
+
+  game_data = () => {
+    console.log(this.state.steamid)
+    fetch('http://localhost:4000/show?steamid=' + this.state.steamid)
+    .then(response => response.json())
+    .then(response => this.setState({usergames: response.data}))
+    .catch(err => console.log(err))
+    console.log(this.state.usergames[0])
+  }
+  
+  render() {
+    const {steamid} = this.state
+    return (
+      <div className="App">
+        <input 
+        value={steamid}
+        onChange={e => this.setState({steamid: e.target.value})} />
+        <button onClick={this.game_data}>Search</button>
+        
+        {this.state.usergames.map((Person, Index) => 
+          <div>
+            <p>SteamId64: {Person.SteamId64}</p>
+            <p>UserName: {Person.UserName}</p>
+            <p>GameId: {Person.GameId}</p>
+            <p>PlayTime: {Person.PlayTime}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
