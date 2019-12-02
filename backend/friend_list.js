ea6d2e3241aa33c4c449bf1e4c45d162f290324b
Peilin Rao <pelinium@gmail.com>
2019-12-01 21:47:46 -0600

    OK

---
+++ backend/friend_list.js
@@ -0,0 +1,51 @@
+module.exports = {
+    API_friend_list: function(steamid){
+      // var MongoClient = require('mongodb').MongoClient;
+      // var url = "mongodb://localhost:27017/";
+      // MongoClient.connect(url, function(err, client) {
+      //   if (err) throw err;
+      //   var db = client.db("text");
+      //   var myobj = { name: "Company Inc", address: "Highway 37" };
+      //   db.collection("customers").insertOne(myobj, function(err, res) {
+      //     if (err) throw err;
+      //     console.log("1 document inserted");
+      //     client.close();
+      //   });
+      // });
+      console.log("Getting your friends:",steamid)
+      const fetch = require('node-fetch')
+      key = "AA7FA6275849EC957DF95C8DE0945CB7";
+      function APIGetFriendList(key,steamid) {
+        return fetch("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key="+key+"&steamid="+steamid+"&relationship=friend") // Call the fetch function passing the url of the API as a parameter
+        .then((resp) => resp.json())
+      }
+      async function insert_to_mongo(listOfAll){
+        console.log(listOfAll)
+        friend_list = listOfAll.friendslist.friends;
+        console.log(friend_list.length)
+        var MongoClient = require('mongodb').MongoClient;
+        var url = "mongodb://localhost:27017/";
+        MongoClient.connect(url, function(err, client) {
+          if (err) throw err;
+          var db = client.db("steamining");
+          var arr = []
+          for(var i = 0; i < friend_list.length; i++){
+            var myobj = { steamid: friend_list[i].steamid, friend_since: friend_list[i].friend_since };
+            arr.push(myobj)
+          }
+          // console.log(arr)
+          var obj = { mysteam: steamid, friend_list:arr};
+          console.log(obj);
+          db.collection("friend_list").update(obj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            client.close();
+          });
+        });
+      };
+
+      APIGetFriendList(key,steamid).then(listOfAll => insert_to_mongo(listOfAll));
+
+    }
+
+};
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 var users_table = require('./users_table');
 var games_table = require('./games_table');
 var visitors_table = require('./visitors_table');
+var friend_list = require('./friend_list');
 
 const express = require('express');
 const cors = require('cors');
@@ -15,43 +16,22 @@ var con = mysql.createConnection({
   password:'UIuc7355608!!',
   database:'STEAMINING'
 });
-
 con.connect(err => {if(err) {return err;}})
 app.use(cors());
-
-
-app.get('/',(req,res)=>{
-  res.send('hello from the other side (backend)');
-});
-
-// app.get('/test_apt_getgames',(req,res)=>{
-//   res.send("Updating game table");
-//   games_table.API_get_games();
-//   res.send("Updated game table!");
-// });
-//
 app.get('/update_user_database',(req,res)=>{
   const{ steamid } = req.query;
   users_table.API_users_add(steamid);
   res.send("You are added!");
 })
-//
-// app.get('/delete_from_database',(req,res)=>{
-//   const{ steamid } = req.query;
-//   users_table.API_users_delete(steamid);
-//   res.send("You are deleted!");
-// })
-//
 app.get('/update_game_database',(req,res)=>{
   const{ steamid } = req.query;
   games_table.API_get_games(steamid);
   res.send("You are updated!");
 })
 
+
 app.get('/search', (req, res) => {
-
   const {steamid} = req.query;
-
   const VIEW_GAMES_FOR_ID_QUERY = "SELECT COUNT(GameId) FROM STEAMINING.USERS WHERE SteamId64 = " + steamid + " GROUP BY SteamId64; SELECT SUM(PlayTime) FROM STEAMINING.USERS WHERE SteamId64 = " + steamid + " GROUP BY SteamId64; SELECT SUM(G.price) FROM STEAMINING.USERS as U, (SELECT DISTINCT appid, price FROM STEAMINING.GAMES) as G WHERE U.GameId = G.appid and U.SteamId64 = " + steamid + " GROUP BY U.SteamId64; SELECT G.name, U.PlayTime FROM STEAMINING.USERS as U, (SELECT DISTINCT appid, name FROM STEAMINING.GAMES) as G WHERE U.GameId = G.appid and U.SteamId64 = " + steamid + " ORDER BY U.PlayTime DESC Limit 5;"
   con.query(VIEW_GAMES_FOR_ID_QUERY, (err, results) => {
     if(err) {return res.send(err)}
@@ -60,8 +40,14 @@ app.get('/search', (req, res) => {
     }
   });
 })
-
-//visitors_table.API_total_games("76561198269300487");
 app.listen(4000,()=>{
   console.log('backend listening on 4000');
+})
+
+//--------------------------mongodb starts here--------------------------
+
+app.get('/find_my_friends',(req,res)=>{
+  const{ steamid } = req.query;
+  friend_list.API_friend_list(steamid);
+  res.send("Your friends updated!");
 })
